deba840499ef1dc33cd5f5c150e5820e420cdae5
woo215 <108974242+woo215@users.noreply.github.com>
2022-07-09 17:36:15 +0900

    10773 풀이 중

--- github-1.cpp
+++ github-1.cpp
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main(){
-	printf("Hello World");
+	int k, i, n, sum = 0;
+	
+	scanf("%d", &k);
+	
+	for(i=0;i<k;i++){
+		scanf("%d", &n);
+		if(k==0){
+			sum -= n;
+		}
+		else{
+			sum += n;
+		}
+	}
+	printf("%d", sum);
 	return 0;
 }
